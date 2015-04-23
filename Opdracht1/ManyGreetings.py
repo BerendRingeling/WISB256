ad49fbfb2a1f2b6f5bb51ed57f4742d51378866f
berendringeling <berend.ringeling@hotmail.com>
2015-04-23 13:55:12 +0000

    eindeind

--- Opdracht1/ManyGreetings.py
+++ Opdracht1/ManyGreetings.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-y=input('What is your name? ')
-print('Hello, '+y+'!')
-=======
-y=input('Name? ')
+y=input('What is your Name? ')
 z=input('How many greetings? ')
 print(z+' x Hello, '+y+'!')
->>>>>>> extension
+
