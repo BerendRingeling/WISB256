5318469ff90a2424ff1e86f928ac0c91540291d8
berendringeling <berend.ringeling@hotmail.com>
2015-05-08 11:45:52 +0000

    primesbonus

---
+++ Opdracht2/CountPrimes.py
@@ -0,0 +1,35 @@
+import math
+
+count=0
+for line in open('prime.dat'):
+    count=count+1
+
+a=[]
+for line in open('prime.dat'):
+    a=a+[int(line)]
+
+z=[0]*count
+j=0
+count2=0
+while j<count-1:
+    if a[j+1]-a[j]==2:
+        z[j]=a[j]
+        count2=count2+1
+    j=j+1
+    
+p=list((set(z)))
+p.remove(0)
+p.sort()
+M=int(line)/(math.log(int(line)))
+C=0.6601618
+
+
+print('Largest Prime =',int(line))
+print('--------------------------------')
+print('pi(N)         =',count)
+print('N/Log(N)      =',M)
+print('ratio         =',(1/M)*count)
+print('--------------------------------')
+print('pi_2(N)       =',count2)
+print('2CN/log(N)^2  =',2*C*int(line)/(math.log(int(line))**2))
+print('ratio         =',count2*(1/(2*C*int(line)/(math.log(int(line))**2))))
